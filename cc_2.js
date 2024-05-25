3cb38a580ca4572a421cc5e11f0cbfcdb3a1d3f2
Daniel Wilson <danwilson501@gmail.com>
2024-05-24 23:45:23 -0400

    Acceleration and Braking methods added. Increases speed by 10 km/h and decreases speed by 5km/h.

--- cc_2.js
+++ cc_2.js
@@ -4,4 +4,14 @@ class Car {
         this.make = make;
         this.speed = speed;
     }
+    
+    accelerate() {
+        this.speed += 10;
+        console.log(`The ${this.make} is now going at ${this.speed} km/h.`);
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`The ${this.make}, is now going at ${this.speed}, km/h.`);
+    }
 }
