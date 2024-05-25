9848091a02fba054fcde334ee692a7ee34afacba
Daniel Wilson <danwilson501@gmail.com>
2024-05-24 23:46:41 -0400

    Tested acceleration abd deceleration on both car data sets. Returned successful. Added comment how to input into console.

--- cc_2.js
+++ cc_2.js
@@ -15,3 +15,7 @@ class Car {
         console.log(`The ${this.make}, is now going at ${this.speed}, km/h.`);
     }
 }
+const car1 = new Car("BMW", 120);
+const car2 = new Car("Mercedes", 95);
+
+//In console, enter car1.accelerate(); or car1.brake(); in order to test the car data provided and view calculations.
